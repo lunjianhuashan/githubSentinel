746aa6a45af6d9c9273d22688d7b1e64be73f558
Leo <1946025298@qq.com>
2025-02-18 18:23:25 +0800

    style

--- src/main.py
+++ src/main.py
@@ -1,18 +1,39 @@
 import time
+import json
 from github_client import GitHubClient
 from notifier import Notifier
 from reporter import Reporter
 from subscriber import Subscriber
-from config import config
+# from config import config
+import requests
+
+# proxies = {
+#     "http": "http://104.234.0.191:port",  # 替换为你的代理地址
+#     "https": "https://104.234.0.191:port",  # 替换为你的代理地址
+# }
+
+# 在请求时使用代理
+# response = requests.get("https://www.google.com")
+
+
+# 加载配置文件
+with open('config/config.json') as f:
+    config = json.load(f) 
+
+github_client = GitHubClient(config['github']['api_token'])
+subscriber = Subscriber(config['github']['repos'])
+notifier = Notifier(config['notifications'])
+reporter = Reporter()
 
 def main():
-    github_client = GitHubClient(config['github']['api_token'])
-    subscriber = Subscriber(config['github']['repos'])
-    notifier = Notifier(config['notifications'])
-    reporter = Reporter()
+    print("Starting the GitHub Sentinel...")
 
     while True:
         updates = github_client.get_updates(subscriber.get_subscribed_repos())
+        report = reporter.generate_report(updates)
+        notifier.send_notification(report)
+
+          # 生成报告并发送通知
         report = reporter.generate_report(updates)
         notifier.send_notification(report)
         
